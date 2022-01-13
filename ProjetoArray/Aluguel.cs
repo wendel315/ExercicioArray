3fcc06391401a610b17daa98e626b25604ba6a56
Wendel Marins <wendeldemarins@hotmail.com>
2022-01-13 20:37:46 -0300

    Exercicio Array

---
+++ ProjetoArray/Aluguel.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjetoArray
+{
+    internal class Aluguel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Aluguel (string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
+
+    
---
+++ ProjetoArray/Lists.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ProjetoArray
+{
+    internal class Lists
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            string[] line = Console.ReadLine().Split(' ');
+            int M  = int.Parse(line[0]);
+            int N = int.Parse(line[1]);
+
+            int[,] mat = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i =0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (j < N - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (i < M - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
